df69298a65b842a83e21a07f1798275040ac673c
Prashant Kumar <programmerkrprashant@gmail.com>
2025-11-19 20:43:59 +0530

    add new lession

---
+++ 08_Node.js_Streams_Handling_Data_Flow_like_a_Pro/16_Writing_1_Lakh_Numbers/app.js
@@ -0,0 +1,47 @@
+// Write 1 lakh num using nodejs
+
+import fs from 'fs'
+
+console.time()
+// It does not write number in correct order
+
+// for (let i = 1; i <=100000; i++) {
+//   if (i === 1) {
+//     fs.writeFileSync('num.txt', `${i}, `, err => {
+//       if (err) console.log(err)
+//       if (i === 100000) {
+//         console.timeEnd()
+//       }
+//     })
+//   } else {
+//     fs.appendFileSync('num.txt', `${i}, `, err => {
+//       if (err) console.log(err)
+//       if (i === 100000) {
+//         console.timeEnd()
+//       }
+//     })
+//   }
+// }
+// console.timeEnd() // It's taking too much time
+// It write numbers in corre order
+// Time : 1.312 s
+for (let i = 1; i <= 100000; i++) {
+  if (i === 1) {
+    fs.writeFileSync('num.txt', `${i}, `)
+  } else {
+    fs.appendFileSync('num.txt', `${i}, `)
+  }
+}
+
+console.timeEnd()
+
+// Time : 32 ms
+// const writeStream = fs.createWriteStream('streamNum.txt')
+
+// for (let i = 1; i <= 1000000; i++) {
+//   writeStream.write(`${i}, `)
+// }
+// writeStream.end()
+// writeStream.on('finish', () => {
+//   console.timeEnd() // 3 s , 1000ms = 1s
+// })
