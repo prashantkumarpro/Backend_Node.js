2393354c7f8ace5ad8f7a13369ec9ba8352c38ab
Prashant Kumar <programmerkrprashant@gmail.com>
2026-02-11 21:01:02 +0530

    add detailedItem

--- 10_Networking_With_Core_NodeJS_Modules/22_Creating_Online_Storage_Platform_Like_Google_Drive/app.js
+++ 10_Networking_With_Core_NodeJS_Modules/22_Creating_Online_Storage_Platform_Like_Google_Drive/app.js
@@ -1,40 +1,73 @@
 import { open, readdir, readFile } from 'fs/promises'
 import http from 'http'
+import mime from 'mime-types'
+import path from 'node:path'
 
 const server = http.createServer(async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
   if (req.url === '/') {
-    serverDirectory(req, res)
-  } else {
-    try {
-      const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`)
+    await serverDirectory(req, res)
+    return
+  }
 
-      const stats = await fileHandle.stat()
-      if (stats.isDirectory()) {
-        serverDirectory(req, res)
-        return
-      }
+  try {
+    const [url, queryString] = req.url.split('?')
 
-      const readStream = fileHandle.createReadStream()
-      readStream.pipe(res)
-    } catch (error) {
-      console.log(error.message)
-      res.end('NOt Found')
+    const queryPrams = {}
+    if (queryString) {
+      queryString.split('&').forEach(pair => {
+        const [key, value] = pair.split('=')
+        queryPrams[key] = value
+      })
     }
+
+    const fileHandle = await open(`./storage${decodeURIComponent(url)}`)
+    const stats = await fileHandle.stat()
+
+    if (stats.isDirectory()) {
+      await serverDirectory(req, res)
+      return
+    }
+
+    const readStream = fileHandle.createReadStream()
+
+    res.setHeader(
+      'Content-Type',
+      mime.contentType(url.slice(1)) || 'application/octet-stream'
+    )
+
+    res.setHeader('Content-Length', stats.size)
+
+    if (queryPrams.action === 'download') {
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename="${path.basename(url)}"`
+      )
+    }
+
+    readStream.pipe(res)
+  } catch (error) {
+    console.log(error.message)
+    res.statusCode = 404
+    res.end('Not Found')
   }
 })
 
 async function serverDirectory (req, res) {
-  const itemList = await readdir(`./storage${req.url}`)
-  let dynamicHTML = ''
-  itemList.forEach(item => {
-    dynamicHTML += `<li><a href="${
-      req.url === '/' ? '' : req.url
-    }/${item}">${item}</a></li>`
+  const [url] = req.url.split('?')
+  const itemList = await readdir(`./storage${url}`, {
+    withFileTypes: true
   })
-  const htmlBiolerplate = await readFile('./biolerplate.html', 'utf-8')
-  res.end(htmlBiolerplate.replace('${dynamicHTML}', dynamicHTML))
+
+  const detailedList = itemList.map(item => ({
+    name: item.name,
+    type: item.isDirectory() ? 'folder' : 'file'
+  }))
+
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(detailedList))
 }
 
-server.listen(4000, '0.0.0.0', () => {
+server.listen(80, '172.30.85.211', () => {
   console.log('Server started')
 })
