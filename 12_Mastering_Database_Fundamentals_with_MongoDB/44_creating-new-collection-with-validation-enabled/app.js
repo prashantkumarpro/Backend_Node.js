9c297ba791859052d2d0bc7b72b1d13d1151b0b7
Prashant Kumar <programmerkrprashant@gmail.com>
2026-05-03 15:52:22 +0530

    creating new collection with validation

---
+++ 12_Mastering_Database_Fundamentals_with_MongoDB/44_creating-new-collection-with-validation-enabled/app.js
@@ -0,0 +1,54 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+
+await client.connect();
+
+const db = client.db();
+
+await db.createCollection("users", {
+  validator: {
+    name: {
+      $type: "string",
+    },
+    age: {
+      $type: "int",
+    },
+  },
+});
+
+// await db.command({
+//   create: "users",
+//   validator: {
+//     name: {
+//       $type: "string",
+//     },
+//     age: {
+//       $type: "int",
+//     },
+//   },
+// });
+
+// await db.command({
+//   collMod: "users",
+//   validator: {
+//     name: {
+//       $type: "string",
+//     },
+//     age: {
+//       $type: "int",
+//     },
+//   },
+//   validationAction: "warn",
+// });
+
+const collections = await db.listCollections().toArray();
+console.log(collections[0].options);
+
+// try {
+//   await collection.insertOne({ age: 80 });
+// } catch (err) {
+//   console.log(err);
+// }
+
+client.close();
