a47e84430dd393022e88f5b37412bb0539930469
Prashant Kumar <programmerkrprashant@gmail.com>
2026-05-30 16:18:20 +0530

    add new lession

--- 12_Mastering_Database_Fundamentals_with_MongoDB/59_authentication-and-authorization-in-mongodb/app.js
+++ 12_Mastering_Database_Fundamentals_with_MongoDB/59_authentication-and-authorization-in-mongodb/app.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 // const connectionUrl = "mongodb://anurag:anurag@127.0.0.1:27018/storageApp";
 const connectionUrl =
-  "mongodb://ram:ram@127.0.0.1:27018/storageApp?authSource=admin";
+  "mongodb://dataDock:dataDock%40108@ec2-3-92-51-247.compute-1.amazonaws.com/dataDock";
   
 const client = new MongoClient(connectionUrl);
 
