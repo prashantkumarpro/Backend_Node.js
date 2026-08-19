4cfa016583a74f783681c5740bc4e2daef11a340
Prashant Kumar <programmerkrprashant@gmail.com>
2026-08-19 19:30:46 +0530

    add new lession

---
+++ 14_Mastering_Authentication_and_Authorization_in_Node.js/18_using-bcrypt-to-hash-passwords/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import User from "../models/userModel.js";
+
+export default async function checkAuth(req, res, next) {
+  const { token } = req.signedCookies;
+
+  if (!token) {
+    res.clearCookie("token");
+    return res.status(401).json({ error: "Not logged in!" });
+  }
+
+  const { id, expiry: expiryTimeInSeconds } = JSON.parse(
+    Buffer.from(token, "base64url").toString()
+  );
+
+  const currentTimeInSeconds = Math.round(Date.now() / 1000);
+  if (currentTimeInSeconds > expiryTimeInSeconds) {
+    res.clearCookie("token");
+    return res.status(401).json({ error: "Not logged in!" });
+  }
+
+  const user = await User.findOne({ _id: id }).lean();
+  if (!user) {
+    return res.status(401).json({ error: "Not logged in!" });
+  }
+  req.user = user;
+  next();
+}
